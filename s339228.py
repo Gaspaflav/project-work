a3dbb3e25b7f56571fced17c72f9709344818604
Flavio Gasparini <gaspariniflavio2002@gmail.com>
2026-01-19 23:38:09 +0100

    last modify

--- s339228.py
+++ s339228.py
@@ -1,25 +1,10 @@
-"""
-Solution Module for Gold Collection Problem
-Student ID: 339228
 
-This module contains the main solution function that solves the gold collection problem
-using an adaptive hybrid algorithm combining Genetic Algorithm and Hill Climbing.
-"""
 
 from src.solver import adaptive_solver, conversion_solution
 
 
 def solution(p):
-    """
-    Solve the gold collection problem instance.
-    
-    Args:
-        p: Problem instance with graph, parameters (alpha, beta, density), 
-           and methods (cost, baseline, graph properties)
-    
-    Returns:
-        List of (node, gold_amount) tuples representing the solution path
-    """
+
     # Call the adaptive solver to find best path and trip counts
     best_path, optimized_trip_counts, final_cost = adaptive_solver(p)
     
